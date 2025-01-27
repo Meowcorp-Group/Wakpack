ed3ee6fe14a22dd7be151c65f4e57bf8abd8d45e
sitasreturn <87218882+sitasreturn@users.noreply.github.com>
2025-01-27 16:27:16 +0700

    format

--- kubejs/startup_scripts/sillystuffs.js
+++ kubejs/startup_scripts/sillystuffs.js
@@ -1,18 +1,20 @@
-StartupEvents.registry('item', event => {
+StartupEvents.registry('item', (event) => {
+	// Orespawn shit lmao
+	event
+		.create('nether_star_ingot')
+		.displayName('Nether Star Ingot')
+		.texture('kubejs:item/orespawn/nether_star_ingot');
 
-  // Orespawn shit lmao
-event.create('nether_star_ingot')
-.displayName('Nether Star Ingot')
-.texture('kubejs:item/orespawn/nether_star_ingot')
+	event
+		.create('fluxed_uranium_ingot')
+		.displayName('Activated Uranium Ingot')
+		.texture('kubejs:item/orespawn/fluxed_uranium_ingot');
 
-event.create('fluxed_uranium_ingot')
-.displayName('Activated Uranium Ingot')
-.texture('kubejs:item/orespawn/fluxed_uranium_ingot')
-
-event.create('titanium_uranium_sword', 'sword')
-.displayName('Ultimate Sword')
-.tier('netherite')
-.attackDamageBaseline(195.0)
-.maxDamage(5000)
-.texture('kubejs:item/orespawn/titanium_uranium_sword.png')
-  })
+	event
+		.create('titanium_uranium_sword', 'sword')
+		.displayName('Ultimate Sword')
+		.tier('netherite')
+		.attackDamageBaseline(195.0)
+		.maxDamage(5000)
+		.texture('kubejs:item/orespawn/titanium_uranium_sword.png');
+});
