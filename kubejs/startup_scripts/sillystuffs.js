3d78b8b2814173c35747fc8e9e4f1f2df9f6d66c
Waoweens <30313089+Waoweens@users.noreply.github.com>
2025-01-27 16:28:28 +0700

    remove explicit texture references

--- kubejs/startup_scripts/sillystuffs.js
+++ kubejs/startup_scripts/sillystuffs.js
@@ -3,12 +3,10 @@ StartupEvents.registry('item', (event) => {
 	event
 		.create('nether_star_ingot')
 		.displayName('Nether Star Ingot')
-		.texture('kubejs:item/orespawn/nether_star_ingot');
 
 	event
 		.create('fluxed_uranium_ingot')
 		.displayName('Activated Uranium Ingot')
-		.texture('kubejs:item/orespawn/fluxed_uranium_ingot');
 
 	event
 		.create('titanium_uranium_sword', 'sword')
@@ -16,5 +14,4 @@ StartupEvents.registry('item', (event) => {
 		.tier('netherite')
 		.attackDamageBaseline(195.0)
 		.maxDamage(5000)
-		.texture('kubejs:item/orespawn/titanium_uranium_sword.png');
 });
